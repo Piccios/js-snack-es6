ff10593237b6ef4cd9a24804f905150b9859d4bb
Piccio <lorenzo.hl.picchi@gmail.com>
2024-05-23 16:00:26 +0200

    array "tavoloVip" set

---
+++ Js/script.js
@@ -0,0 +1,57 @@
+const tavoloVip = [
+    {
+        tavolo: "TavoloVip",
+        nome: 'Brad Pitt',
+        posto: 1
+    },
+    {
+        tavolo: "TavoloVip",
+        nome: 'Johny Depp',
+        posto: 2
+    },
+    {
+        tavolo: "TavoloVip",
+        nome: 'Lady Gaga',
+        posto: 3
+    },
+    {
+        tavolo: "TavoloVip",
+        nome: 'Cristiano Ronaldo',
+        posto: 4
+    },
+    {
+        tavolo: "TavoloVip",
+        nome: 'Georgina Rodriguez',
+        posto: 5
+    },
+    {
+        tavolo: "TavoloVip",
+        nome: 'Chiara Ferragni',
+        posto: 6
+    },
+    {
+        tavolo: "TavoloVip",
+        nome: 'George Clooney',
+        posto: 7
+    },
+    {
+        tavolo: "TavoloVip",
+        nome: 'Amal Clooney',
+        posto: 8
+    },
+    {
+        tavolo: "TavoloVip",
+        nome: 'Fedez',
+        posto: 9
+    },
+    {
+        tavolo: "TavoloVip",
+        nome: 'Amadeus',
+        posto: 10
+    },
+    {
+        tavolo: "TavoloVip",
+        nome: 'Fiorello',
+        posto: 11
+    },
+]
