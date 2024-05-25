4fc1ffb33eaf5bb79c351b854846d31fb2de773c
Piccio <lorenzo.hl.picchi@gmail.com>
2024-05-25 14:55:54 +0200

    array of objects (bikes) set

---
+++ Snack_3-4/js/script.js
@@ -0,0 +1,28 @@
+const bikes = [
+    {
+    name: "mountain bike",
+    weight: 35
+    },
+    {
+    name: "BMX",
+    weight: 20
+    },
+    {
+    name: "racing",
+    weight: 15
+    },
+    {
+    name: "city bike",
+    weight: 18
+    },
+    {
+    name: "electric",
+    weight: 39
+    },
+    {
+    name: "risciò",
+    weight: 95
+    },
+]
+
+// stampare in console la bicicletta con peso minore utilizzando destructuring e template literal
