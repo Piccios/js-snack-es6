0abfe27d0dbe171a07da672e8e74c9b7bac90232
Piccio <lorenzo.hl.picchi@gmail.com>
2024-05-25 15:00:12 +0200

    Found the bike with the minimum weight setting a const "lightestBike"

--- Snack_3-4/js/script.js
+++ Snack_3-4/js/script.js
@@ -26,3 +26,7 @@ const bikes = [
 ]
 
 // stampare in console la bicicletta con peso minore utilizzando destructuring e template literal
+
+const lightestBike = bikes.reduce((minBike, currentBike) => {
+    return currentBike.weight < minBike.weight ? currentBike : minBike;
+});
