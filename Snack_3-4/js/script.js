2c81df66812e1462449fad1904f20baeb01919bf
Piccio <lorenzo.hl.picchi@gmail.com>
2024-05-25 15:06:13 +0200

    set an array of football teams

--- Snack_3-4/js/script.js
+++ Snack_3-4/js/script.js
@@ -1,3 +1,5 @@
+// snack 3 //
+
 const bikes = [
     {
     name: "mountain bike",
@@ -32,3 +34,36 @@ const lightestBike = bikes.reduce((minBike, currentBike) => {
 const { name, weight } = lightestBike;
 
 console.log(`The bike with the least weight is ${name} with a weight of ${weight} kg.`);
+
+
+//  snack 4 //
+
+const squadreCalcio = [
+    {
+        nome: "Juventus",
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: "Milan",
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: "Inter",
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: "Roma",
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: "Napoli",
+        puntiFatti: 0,
+        falliSubiti: 0
+    }
+];
+
+// assegnare dei valori alle proprieta' settate a 0  utilizzare una funzione che assegni i numeri randomicamente
